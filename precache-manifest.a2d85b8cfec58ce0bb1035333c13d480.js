ce306b6be1dd152b554a038d6b17ed24fa7a1083
Ritik Bhardwaj <ritik.bhardwaj889@gmail.com>
2020-02-24 12:11:53 +0530

    Updates

---
+++ precache-manifest.a2d85b8cfec58ce0bb1035333c13d480.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c89c572e6d3f8147558bb861c3e6aa8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c804c18b09f98d75d50",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "306be7440268bd346a67",
+    "url": "/static/css/main.de2e207c.chunk.css"
+  },
+  {
+    "revision": "4c804c18b09f98d75d50",
+    "url": "/static/js/2.62179776.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.62179776.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "306be7440268bd346a67",
+    "url": "/static/js/main.287b6760.chunk.js"
+  },
+  {
+    "revision": "f71324b6340e3d0e359b",
+    "url": "/static/js/runtime-main.88149351.js"
+  }
+]);
